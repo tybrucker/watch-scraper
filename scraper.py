9b7cded3ae147b33cb239827ae57fa24b7d4cf21
tybrucker <c4brucker@gmail.com>
2023-05-19 15:34:46 -0400

    added comments to work on when home for weekend

--- scraper.py
+++ scraper.py
@@ -2,19 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'https://www.chrono24.com/iwc/pilots-watch-timezoner-top-gun-edition-iw395505-limited--id28255089.htm'
+
+#understand headers arg better and specify what needed
 headers = {"User-Agent":"Mozilla/5.0"}
 html = requests.get(url, headers=headers)
 page = BeautifulSoup(html.content, 'html.parser')
 
+#possibly change to find better way of getting tables needed instead of all tables
 info_tables = page.find_all('table')
 
 
 #use bs4 navigation features
 for table in info_tables:
+    #bodys would be children to tables
     sections = table.find_all('tbody')
     for section in sections:
+        #rows would be children to bodys
         rows = section.find_all('tr')
         for row in rows:
+            #elements (td's) would be children to rows
             elements = row.find_all('td')
             for element in elements:
                 print(element.text)
