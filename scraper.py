9a8773c1de982e00a1ba2cbf45f2fa4d429c993c
tybrucker <c4brucker@gmail.com>
2023-05-25 19:30:37 -0400

    revamped parsing the tree and can now obtain key item pairings changed scope to just using basic info table, other tables contained redundant or not particularly useful information

--- scraper.py
+++ scraper.py
@@ -3,24 +3,27 @@ import requests
 
 url = 'https://www.chrono24.com/iwc/pilots-watch-timezoner-top-gun-edition-iw395505-limited--id28255089.htm'
 
-#understand headers arg better and specify what needed
+# understand headers arg better and specify what needed
 headers = {"User-Agent":"Mozilla/5.0"}
 html = requests.get(url, headers=headers)
 page = BeautifulSoup(html.content, 'html.parser')
 
-#possibly change to find better way of getting tables needed instead of all tables
-info_tables = page.find_all('table')
+# searches for table that contains "Basic Info" text
+basic_info_table = page.find_all(lambda tag: tag.name =='tbody' and "Basic Info" in tag.text)[0]
+# gets rows of basic info table
+table_rows = basic_info_table.find_all('tr')
 
+# parses each row for td tags and puts pairings into lists
+datalist = []
+for row in table_rows:
+    elements = row.find_all('td')
+    try:
+        elementlist.append([element.text.strip() for element in elements])
+    except:
+        print("error")
 
-#use bs4 navigation features
-for table in info_tables:
-    #bodys would be children to tables
-    sections = table.find_all('tbody')
-    for section in sections:
-        #rows would be children to bodys
-        rows = section.find_all('tr')
-        for row in rows:
-            #elements (td's) would be children to rows
-            elements = row.find_all('td')
-            for element in elements:
-                print(element.text)
+# todo:
+# work on element pairing, try to use dicts or touples
+# get rid of basic info sublist since we know its the basic info table
+# work on general mapping of key item pairings
+# consistent structure to get ready for multiple pages
