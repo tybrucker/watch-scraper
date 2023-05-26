d554489ee4a6a857c3659594788dbfedd2376efb
tybrucker <c4brucker@gmail.com>
2023-05-26 14:36:34 -0400

    now parsing all watch links in a list page, updated todo list for next session

--- scraper.py
+++ scraper.py
@@ -3,13 +3,18 @@ import requests
 
 # understand headers arg better and specify what needed
 headers = {"User-Agent":"Mozilla/5.0"}
-
 chrono24 = 'https://www.chrono24.com'
 
 single_watch_url = 'https://www.chrono24.com/rolex/rolex-rolex-gmt-master-ii-batman-oyster-bracelet-126710blnr-2023--id20505792.htm'
 watch_list_url = 'https://www.chrono24.com/rolex/index.htm'
 
 def parse_watch_page(url):
+    """
+    function to parse a single watch page
+    :param url: url to a singular watch page
+    :return: a list of descriptor/info tuples
+    """
+
     # get page as bs4 html parser
     html = requests.get(url, headers=headers)
     page = BeautifulSoup(html.content, 'html.parser')
@@ -29,24 +34,28 @@ def parse_watch_page(url):
     return datalist
 
 def parse_watch_list(url):
+    """
+    function to parse a watch list page
+    :param url: url to a page to a list of watches and next butt
+    :return: link to next?
+    """
+
     # get page as bs4 html parser
     html = requests.get(url, headers=headers)
     page = BeautifulSoup(html.content, 'html.parser')
 
     # finds all links to watches on the list
     watches = page.find_all('a', 'article-item block-item rcard')
-    print(parse_watch_page(chrono24 + watches[0]['href']))
-
-    # for watch in watches:
-        # print(i['href'])
+    # parses each watch page
+    for watch in watches:
+        print(parse_watch_page(chrono24 + watch['href']))
 
 parse_watch_list(watch_list_url)
 
-
-
-
-
-# todo:
-# work on general mapping of key item pairings
-# consistent structure to get ready for multiple pages
-# MAKE MORE COMMITS WHILE WORKING, TOO MUCH CHANGING PER COMMIT
+"""
+todo
+- try to speed the mf up, possibly slow due to bad internet
+- finalize parse_watch_list:
+    - store data from pages appropriately
+    - get next link
+"""
