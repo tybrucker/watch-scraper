764cf0c42fbdde6dd325bb169ed2bea47d420995
tybrucker <c4brucker@gmail.com>
2023-05-26 14:25:47 -0400

    got links to watches on the list page, need to parse all and store them somehow, then get the next page

--- scraper.py
+++ scraper.py
@@ -1,12 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.chrono24.com/rolex/rolex-rolex-gmt-master-ii-batman-oyster-bracelet-126710blnr-2023--id20505792.htm'
-
 # understand headers arg better and specify what needed
 headers = {"User-Agent":"Mozilla/5.0"}
 
+chrono24 = 'https://www.chrono24.com'
+
+single_watch_url = 'https://www.chrono24.com/rolex/rolex-rolex-gmt-master-ii-batman-oyster-bracelet-126710blnr-2023--id20505792.htm'
+watch_list_url = 'https://www.chrono24.com/rolex/index.htm'
+
 def parse_watch_page(url):
+    # get page as bs4 html parser
     html = requests.get(url, headers=headers)
     page = BeautifulSoup(html.content, 'html.parser')
     
@@ -23,6 +27,22 @@ def parse_watch_page(url):
         datalist.append(test)
     datalist.pop(0)
     return datalist
+
+def parse_watch_list(url):
+    # get page as bs4 html parser
+    html = requests.get(url, headers=headers)
+    page = BeautifulSoup(html.content, 'html.parser')
+
+    # finds all links to watches on the list
+    watches = page.find_all('a', 'article-item block-item rcard')
+    print(parse_watch_page(chrono24 + watches[0]['href']))
+
+    # for watch in watches:
+        # print(i['href'])
+
+parse_watch_list(watch_list_url)
+
+
 
 
 
