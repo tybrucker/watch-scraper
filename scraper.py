79ac2d973e38a8eef2b65e16ec0b42e026230835
tybrucker <c4brucker@gmail.com>
2023-06-15 16:26:01 -0400

    changed parser to lxml, updated todo list, pseudocoded brand_handler function

--- scraper.py
+++ scraper.py
@@ -17,7 +17,7 @@ def parse_watch_page(url):
 
     # get page as bs4 html parser
     html = requests.get(url, headers=headers)
-    page = BeautifulSoup(html.content, 'html.parser')
+    page = BeautifulSoup(html.content, 'lxml')
     
     # searches for table that contains "Basic Info" text
     basic_info_table = page.find_all(lambda tag: tag.name =='tbody' and "Basic Info" in tag.text)[0]
@@ -33,22 +33,33 @@ def parse_watch_page(url):
     datalist.pop(0)
     return datalist
 
-def parse_watch_list(url):
+def parse_watch_list(url, brand_list):
     """
     function to parse a watch list page
     :param url: url to a page to a list of watches and next butt
-    :return: link to next?
+    :return: link to next or NULL if none
     """
 
     # get page as bs4 html parser
     html = requests.get(url, headers=headers)
-    page = BeautifulSoup(html.content, 'html.parser')
+    page = BeautifulSoup(html.content, 'lxml')
 
     # finds all links to watches on the list
     watches = page.find_all('a', 'article-item block-item rcard')
     # parses each watch page
     for watch in watches:
-        print(parse_watch_page(chrono24 + watch['href']))
+        # add brand_list.append() to below when implementing brand_handler
+        parse_watch_page(chrono24 + watch['href'])
+    #return link to next page, if no next return NULL
+
+"""
+def brand_handler(url):
+    brand_watches = []
+    link = url
+    while(link != NULL):
+        link = parse_watch_list(link, brand_watches)
+    return brand_watches
+"""
 
 parse_watch_list(watch_list_url)
 
@@ -57,5 +68,8 @@ todo
 - try to speed the mf up, possibly slow due to bad internet
 - finalize parse_watch_list:
     - store data from pages appropriately
-    - get next link
+    - return next link
+- fully implement brand_handler
+- do more brands
+- put in excel sheet
 """
